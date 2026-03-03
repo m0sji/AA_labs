9b120cd4a5004d3cb1e8e53993b4f382b07f0fcf
Eduard <oberst.eduard@gmail.com>
2026-03-03 16:39:58 +0200

    visual representation

---
+++ server.py
@@ -0,0 +1,86 @@
+from flask import Flask, request, jsonify
+import numpy as np
+
+app = Flask(__name__)
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and arr[i] < arr[l]:
+            largest = l
+        if r < n and arr[largest] < arr[r]:
+            largest = r
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(arr, n, largest)
+
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
+
+def selection_sort(arr, n):
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+@app.route('/sort', methods=['POST'])
+def sort():
+    data = request.json
+    algorithm = data['algorithm']
+    array = np.array(data['data'])
+
+    if algorithm == 'quicksort':
+        sorted_array = quicksort(array)
+    elif algorithm == 'mergesort':
+        sorted_array = merge_sort(array)
+    elif algorithm == 'heapsort':
+        sorted_array = heap_sort(array)
+    elif algorithm == 'selectionsort':
+        sorted_array = selection_sort(array, len(array))
+
+    return jsonify({'sortedData': sorted_array.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
